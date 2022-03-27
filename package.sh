61fd36cf450a1817840762bd467b0a717ce1ac0d
root <root@node3851-threecxdev.za.cloudlet.cloud>
2022-03-27 08:25:48 +0000

    Added path parameter

--- package.sh
+++ package.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-version=$1
+path=$1 version=$2
+if [[ ! $path ]]; then path='.'; fi
 if [[ $version ]];
 then
-    docker build -t umlatt/3cx:$version .
+    docker build -t umlatt/3cx:$version $path
 else
     printf "Version number required.\ne.g.\t$0 1.0.2\n"
 fi
-docker run -dit --name threecx.$version umlatt/3cx:$version
+docker run -dit --network host --name threecx.$version umlatt/3cx:$version
 docker exec -it threecx.$version bash
