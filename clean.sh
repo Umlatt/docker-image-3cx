d37037390e9f44ff31b8469898c39e47373eee92
root <root@node3851-threecxdev.za.cloudlet.cloud>
2022-03-27 08:25:11 +0000

    Clean all

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for container in `docker ps -q`; do docker stop $container; docker rm $container; done
-for image in `docker image list -q | grep -v 682f678c3fa0`; do docker rmi $image; done
+for container in `docker ps -q -a`; do docker stop $container; docker rm $container; done
+for image in `docker image list -q | grep -v 682f678c3fa0 | grep -v 71d646d8e086`; do docker rmi $image; done
 docker image list
